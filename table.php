77439a17ad9b9388709ecb2fca35d748a6836686
junepchobplub <98872285+junepchobplub@users.noreply.github.com>
2022-03-07 23:17:19 +0700

    Update table.php

--- table.php
+++ table.php
@@ -13,31 +13,31 @@
       <tr>
         <td>1</td>
         <td>แจฮยอน</td>
-        <td><img src="https://www.instagram.com/p/CV4qncbFdtg/?utm_medium=copy_link" width="45px"></td>
+        <td><img src="https://images.app.goo.gl/fXPqbSgpVbTbeSWA9" width="45px"></td>
         <td><a href="https://instagram.com/_jeongjaehyun?utm_medium=copy_link">Facebook</a></td>
       </tr>
       <tr>
         <td>2</td>
         <td>โดยอง</td>
-        <td><img src="https://www.instagram.com/p/CYgkEuipPJq/?utm_medium=copy_link" width="45px"></td>
+        <td><img src="https://images.app.goo.gl/P2oQ5i12via8djMJA" width="45px"></td>
         <td><a href="https://instagram.com/do0_nct?utm_medium=copy_link">Facebook</a></td>
       </tr>
       <tr>
         <td>3</td>
         <td>มาร์ค</td>
-        <td><img src="https://www.instagram.com/p/CV9WKzyJLPq/?utm_medium=copy_link" width="45px"></td>
+        <td><img src="https://images.app.goo.gl/c374Rppy7y2VVvNdA" width="45px"></td>
         <td><a href="https://instagram.com/onyourm__ark?utm_medium=copy_link">Facebook</a></td>
       </tr>
       <tr>
         <td>4</td>
         <td>จอร์นนี่</td>
-        <td><img src="https://www.instagram.com/p/CYwDRmGhxWQ/?utm_medium=copy_link"45px"></td>
+        <td><img src="https://images.app.goo.gl/YQ9zuNUG2j1uhcdT9"45px"></td>
         <td><a href="https://instagram.com/johnnyjsuh?utm_medium=copy_link">Facebook</a></td>
       </tr>
       <tr>
         <td>5</td>
         <td>แทยง</td>
-        <td><img src="https://www.instagram.com/p/CV4qncbFdtg/?utm_medium=copy_link" width="45px"></td>
+        <td><img src="https://images.app.goo.gl/YgfsSjuvdBsmEAWA9" width="45px"></td>
         <td><a href="https://instagram.com/taeoxo_nct?utm_medium=copy_link">Facebook</a></td>
       </tr>
     </table>
