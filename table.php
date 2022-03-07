394cdcd4184a48c6f13669125971e8b845d42a24
junepchobplub <98872285+junepchobplub@users.noreply.github.com>
2022-03-07 22:57:49 +0700

    Create table.php

---
+++ table.php
@@ -0,0 +1,45 @@
+<html>
+  <head>
+    <title>การสร้างตาราง</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ</th>
+        <th>รูปภาพ</th>
+        <th>Twitter/FB</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>แจฮยอน</td>
+        <td><img src="https://www.instagram.com/p/CV4qncbFdtg/?utm_medium=copy_link" width="45px"></td>
+        <td><a href="https://instagram.com/_jeongjaehyun?utm_medium=copy_link">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>โดยอง</td>
+        <td><img src="https://www.instagram.com/p/CYgkEuipPJq/?utm_medium=copy_link" width="45px"></td>
+        <td><a href="https://instagram.com/do0_nct?utm_medium=copy_link">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>มาร์ค</td>
+        <td><img src="https://www.instagram.com/p/CV9WKzyJLPq/?utm_medium=copy_link" width="45px"></td>
+        <td><a href="https://instagram.com/onyourm__ark?utm_medium=copy_link">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>จอร์นนี่</td>
+        <td><img src="https://www.instagram.com/p/CYwDRmGhxWQ/?utm_medium=copy_link"45px"></td>
+        <td><a href="https://instagram.com/johnnyjsuh?utm_medium=copy_link">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td>แทยง</td>
+        <td><img src="https://www.instagram.com/p/CV4qncbFdtg/?utm_medium=copy_link" width="45px"></td>
+        <td><a href="https://instagram.com/taeoxo_nct?utm_medium=copy_link">Facebook</a></td>
+      </tr>
+    </table>
+  </body>
+</html>
